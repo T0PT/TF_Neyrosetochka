169592897ee03d78ce215b13ab760d95e825cd80
Alesha314 <55951687+T0PT@users.noreply.github.com>
2023-04-03 17:22:22 +0000

    did it from ipad from a car

---
+++ try_plt.py
@@ -0,0 +1,9 @@
+import matplotlib as plt
+import numpy as np
+
+x=np.array(range(0,10))
+
+fig2 = plt.plots
+plt.plot([1, 3, 7, 3, 1], c="green", lw=5)
+plt.title("I am the part of figure 2")
+plt.show()
